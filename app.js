173ffed73c6eb9a2079e445733ad61a681443f02
fati-ma <fatima.atiyya@yahoo.com>
2020-10-08 13:35:35 +0300

    Third problem solved

--- app.js
+++ app.js
@@ -53,9 +53,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
     var sumNums = sum(sum(a, b)[0], c)[0];
-    //var allSum = sumNums[0];
     var mulNums = multiply(multiply(a, b)[0], c)[0];
-    //var allMul = mulNums[0];
     return [sumNums, mulNums, `${a} and ${b} and ${c} sum to ${sumNums}.`, `The product of ${a} and ${b} and ${c} is ${mulNums}.`];
 }
 
